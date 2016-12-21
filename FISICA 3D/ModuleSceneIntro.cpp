f3db2886f4a4efcb30395efe98302b6f916dc740
AlfonsoSanchez <alfonsosp2@tr12.cat>
2016-12-21 17:18:53 +0100

    All Pink

--- FISICA 3D/ModuleSceneIntro.cpp
+++ FISICA 3D/ModuleSceneIntro.cpp
@@ -24,179 +24,223 @@ bool ModuleSceneIntro::Start()
 
 	wall1 = new Cube(70, 10, 10);
 	wall1->SetPos(40, 5, -100);
+	wall1->color = Pink;
 	wall1b = App->physics->AddBody(*wall1, 0);
 
 
 	wall2 = new Cube(70, 10, 10);
 	wall2->SetPos(-40, 5, -100);
+	wall2->color = Pink;
 	wall2b = App->physics->AddBody(*wall2, 0);
 
 	wall3 = new Cube(10, 10, 270);
 	wall3->SetPos(80, 5, 30);
+	wall3->color = Pink;
 	wall3b = App->physics->AddBody(*wall3, 0);
 
 	wall4 = new Cube(10, 10, 270);
 	wall4->SetPos(-80, 5, 30);
+	wall4->color = Pink;
 	wall4b = App->physics->AddBody(*wall4, 0);
 
 	i_wall1 = new Cube(10, 10, 30);
 	i_wall1->SetPos(40, 5, -90);
+	i_wall1->color = Pink;
 	i_wall1b = App->physics->AddBody(*i_wall1, 0);
 
 	i_wall2 = new Cube(10, 10, 30);
 	i_wall2->SetPos(-40, 5, -90);
+	i_wall2->color = Pink;
 	i_wall2b = App->physics->AddBody(*i_wall2, 0);
 
 	i_wall3 = new Cube(50, 10, 10);
 	i_wall3->SetPos(0, 5, -80);
+	i_wall3->color = Pink;
 	i_wall3b = App->physics->AddBody(*i_wall3, 0);
 
 	i_wall4 = new Cube(30, 10, 10);
 	i_wall4->SetPos(50, 5, -80);
+	i_wall4->color = Pink;
 	i_wall4b = App->physics->AddBody(*i_wall4, 0);
 
 	i_wall5 = new Cube(30, 10, 10);
 	i_wall5->SetPos(-50, 5, -80);
+	i_wall5->color = Pink;
 	i_wall5b = App->physics->AddBody(*i_wall5, 0);
 
 	i_wall6 = new Cube(10, 10, 50);
 	i_wall6->SetPos(20, 5, -60);
+	i_wall6->color = Pink;
 	i_wall6b = App->physics->AddBody(*i_wall6, 0);
 
 	i_wall7 = new Cube(50, 10, 10);
 	i_wall7->SetPos(40, 5, -60);
+	i_wall7->color = Pink;
 	i_wall7b = App->physics->AddBody(*i_wall7, 0);
 
 	i_wall8 = new Cube(10, 10, 30);
 	i_wall8->SetPos(-20, 5, -70);
+	i_wall8->color = Pink;
 	i_wall8b = App->physics->AddBody(*i_wall8, 0);
 
 	i_wall9 = new Cube(50, 10, 10);
 	i_wall9->SetPos(-40, 5, -60);
+	i_wall9->color = Pink;
 	i_wall9b = App->physics->AddBody(*i_wall9, 0);
 
 	i_wall10 = new Cube(110, 10, 10);
 	i_wall10->SetPos(-30, 5, -40);
+	i_wall10->color = Pink;
 	i_wall10b = App->physics->AddBody(*i_wall10, 0);
 
 	i_wall11 = new Cube(50, 10, 10);
 	i_wall11->SetPos(60, 5, -40);
+	i_wall11->color = Pink;
 	i_wall11b = App->physics->AddBody(*i_wall11, 0);
 
 	i_wall12 = new Cube(10, 10, 30);
 	i_wall12->SetPos(40, 5, -30);
+	i_wall12->color = Pink;
 	i_wall12b = App->physics->AddBody(*i_wall12, 0);
 
 	i_wall13 = new Cube(50, 10, 10);
 	i_wall13->SetPos(40, 5, -20);
+	i_wall13->color = Pink;
 	i_wall13b = App->physics->AddBody(*i_wall13, 0);
 
 	i_wall14 = new Cube(90, 10, 10);
 	i_wall14->SetPos(-40, 5, -20);
+	i_wall14->color = Pink;
 	i_wall14b = App->physics->AddBody(*i_wall14, 0);
 
 	i_wall15 = new Cube(10, 10, 30);
 	i_wall15->SetPos(0, 5, -10);
+	i_wall15->color = Pink;
 	i_wall15b = App->physics->AddBody(*i_wall15, 0);
 
 	i_wall16 = new Cube(70, 10, 10);
 	i_wall16->SetPos(50, 5, 0);
+	i_wall16->color = Pink;
 	i_wall16b = App->physics->AddBody(*i_wall16, 0);
 
 	i_wall17 = new Cube(70, 10, 10);
 	i_wall17->SetPos(-50, 5, 0);
+	i_wall17->color = Pink;
 	i_wall17b = App->physics->AddBody(*i_wall17, 0);
 
 	i_wall18 = new Cube(10, 10, 30);
 	i_wall18->SetPos(20, 5, 10);
+	i_wall18->color = Pink;
 	i_wall18b = App->physics->AddBody(*i_wall18, 0);
 
 	i_wall19 = new Cube(50, 10, 10);
 	i_wall19->SetPos(40, 5, 20);
+	i_wall19->color = Pink;
 	i_wall19b = App->physics->AddBody(*i_wall19, 0);
 
 	i_wall20 = new Cube(70, 10, 10);
 	i_wall20->SetPos(-30, 5, 20);
+	i_wall20->color = Pink;
 	i_wall20b = App->physics->AddBody(*i_wall20, 0);
 
 	i_wall21 = new Cube(10, 10, 50);
 	i_wall21->SetPos(-60, 5, 40);
+	i_wall21->color = Pink;
 	i_wall21b = App->physics->AddBody(*i_wall21, 0);
 
 	i_wall22 = new Cube(10, 10, 30);
 	i_wall22->SetPos(0, 5, 30);
+	i_wall22->color = Pink;
 	i_wall22b = App->physics->AddBody(*i_wall22, 0);
 
 	i_wall23 = new Cube(90, 10, 10);
 	i_wall23->SetPos(40, 5, 40);
+	i_wall23->color = Pink;
 	i_wall23b = App->physics->AddBody(*i_wall23, 0);
 
 	i_wall24 = new Cube(10, 10, 50);
 	i_wall24->SetPos(-30, 5, 60);
+	i_wall24->color = Pink;
 	i_wall24b = App->physics->AddBody(*i_wall24, 0);
 
 	i_wall25 = new Cube(100, 10, 10);
 	i_wall25->SetPos(15, 5, 60);
+	i_wall25->color = Pink;
 	i_wall25b = App->physics->AddBody(*i_wall25, 0);
 
 	i_wall26 = new Cube(40, 10, 10);
 	i_wall26->SetPos(-45, 5, 80);
+	i_wall26->color = Pink;
 	i_wall26b = App->physics->AddBody(*i_wall26, 0);
 
 	i_wall27 = new Cube(10, 10, 70);
 	i_wall27->SetPos(-60, 5, 110);
+	i_wall27->color = Pink;
 	i_wall27b = App->physics->AddBody(*i_wall27, 0);
 
 	i_wall28 = new Cube(30, 10, 10);
 	i_wall28->SetPos(-50, 5, 140);
+	i_wall28->color = Pink;
 	i_wall28b = App->physics->AddBody(*i_wall28, 0);
 
 	i_wall29 = new Cube(10, 10, 50);
 	i_wall29->SetPos(60, 5, 80);
+	i_wall29->color = Pink;
 	i_wall29b = App->physics->AddBody(*i_wall29, 0);
 
 	i_wall30 = new Cube(60, 10, 10);
 	i_wall30->SetPos(15, 5, 80);
+	i_wall30->color = Pink;
 	i_wall30b = App->physics->AddBody(*i_wall30, 0);
 
 	i_wall31 = new Cube(110, 10, 10);
 	i_wall31->SetPos(10, 5, 100);
+	i_wall31->color = Pink;
 	i_wall31b = App->physics->AddBody(*i_wall31, 0);
 
 	i_wall32 = new Cube(10, 10, 90);
 	i_wall32->SetPos(-10, 5, 120);
+	i_wall32->color = Pink;
 	i_wall32b = App->physics->AddBody(*i_wall32, 0);
 
 	i_wall33 = new Cube(40, 10, 10);
 	i_wall33->SetPos(-25, 5, 120);
+	i_wall33->color = Pink;
 	i_wall33b = App->physics->AddBody(*i_wall33, 0);
 
 	i_wall34 = new Cube(70, 10, 10);
 	i_wall34->SetPos(-40, 5, 160);
+	i_wall34->color = Pink;
 	i_wall34b = App->physics->AddBody(*i_wall34, 0);
 
 	i_wall35 = new Cube(70, 10, 10);
 	i_wall35->SetPos(40, 5, 160);
+	i_wall35->color = Pink;
 	i_wall35b = App->physics->AddBody(*i_wall35, 0);
 
 	i_wall36 = new Cube(10, 10, 50);
 	i_wall36->SetPos(10, 5, 140);
+	i_wall36->color = Pink;
 	i_wall36b = App->physics->AddBody(*i_wall36, 0);
 
 	i_wall37 = new Cube(50, 10, 10);
 	i_wall37->SetPos(30, 5, 100);
+	i_wall37->color = Pink;
 	i_wall37b = App->physics->AddBody(*i_wall37, 0);
 
 	i_wall38 = new Cube(10, 10, 30);
 	i_wall38->SetPos(50, 5, 130);
+	i_wall38->color = Pink;
 	i_wall38b = App->physics->AddBody(*i_wall38, 0);
 
 	i_wall39 = new Cube(30, 10, 10);
 	i_wall39->SetPos(40, 5, 140);
+	i_wall39->color = Pink;
 	i_wall39b = App->physics->AddBody(*i_wall39, 0);
 
 	i_wall40 = new Cube(50, 10, 10);
 	i_wall40->SetPos(30, 5, 120);
+	i_wall40->color = Pink;
 	i_wall40b = App->physics->AddBody(*i_wall40, 0);
 
 	//40, 0, 130
